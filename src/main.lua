061c2d76713f915df7eece5c2059879af720fab9
Brasego <brasego@hotmail.fr>
2025-08-29 01:46:54 +0200

    added a space to the message

--- src/main.lua
+++ src/main.lua
@@ -165,7 +165,7 @@ local function ensureInPath(appName)
   local linkPath = binDir .. "/" .. appName
   if not fs.exists(linkPath) then
     io.open(linkPath, "w"):write(string.format('shell.run("/apps/%s/.main")\n', appName))
-    print(appName .. "Added to path as " .. linkPath)
+    print(appName .. " Added to path as " .. linkPath)
   end
 
 end
